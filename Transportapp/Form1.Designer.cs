e2cdb9bb944e339982059cf37be94976f8681518
Pino Scheiblechner <Pino_Scheiblechner@sluz.ch>
2021-04-14 13:36:20 +0200

    Dokumentation hinzugefügt

--- Transportapp/Form1.Designer.cs
+++ Transportapp/Form1.Designer.cs
@@ -35,7 +35,7 @@ namespace Transportapp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1050, 670);
+            this.ClientSize = new System.Drawing.Size(1102, 651);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
